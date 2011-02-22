6abe012082722a23e7a6d3c1ba0afcd5db49d51b
hannes.hofmann <hannes.hofmann@informatik.uni-erlangen.de>
2011-02-22 16:35:00 +0000

    Minor changes to rct_timer.*

--- src/RabbitCTRunner/source/rct_timer.cpp
+++ src/RabbitCTRunner/source/rct_timer.cpp
@@ -4,6 +4,8 @@
 
 #ifdef __RCT_OS_WINDOWS
 // Windows specific code
+	#include <iostream>
+    #include <time.h>
 
 int gettimeofday(struct timeval* tv, struct timezone* tz)
 {
@@ -11,30 +13,30 @@ int gettimeofday(struct timeval* tv, struct timezone* tz)
     unsigned __int64 tmpres = 0;
     static int tzflag;
 
-    if (NULL != tv)
-    {
-	GetSystemTimeAsFileTime(&ft);
+    if (NULL != tv) {
+		GetSystemTimeAsFileTime(&ft);
 
-	tmpres |= ft.dwHighDateTime;
-	tmpres <<= 32;
-	tmpres |= ft.dwLowDateTime;
+		tmpres |= ft.dwHighDateTime;
+		tmpres <<= 32;
+		tmpres |= ft.dwLowDateTime;
 
-	/*converting file time to unix epoch*/
-	tmpres /= 10;  /*convert into microseconds*/
-	tmpres -= DELTA_EPOCH_IN_MICROSECS; 
-	tv->tv_sec  = (long)(tmpres / 1000000UL);
-	tv->tv_usec = (long)(tmpres % 1000000UL);
+		/*converting file time to unix epoch*/
+		tmpres /= 10;  /*convert into microseconds*/
+		tmpres -= DELTA_EPOCH_IN_MICROSECS; 
+		tv->tv_sec  = (long)(tmpres / 1000000UL);
+		tv->tv_usec = (long)(tmpres % 1000000UL);
     }
 
-    if (NULL != tz)
-    {
-	if (!tzflag)
-	{
-	    _tzset();
-	    ++tzflag;
-	}
-	tz->tz_minuteswest = _timezone / 60;
-	tz->tz_dsttime     = _daylight;
+    if (NULL != tz) {
+		std::cerr << "gettimeofday: Error, timezone argument not supported. Must be NULL." << std::endl;
+#if 0
+		if (!tzflag) {
+			_tzset();
+			++tzflag;
+		}
+		tz->tz_minuteswest = _timezone / 60;
+		tz->tz_dsttime     = _daylight;
+#endif
     }
 
     return 0;
