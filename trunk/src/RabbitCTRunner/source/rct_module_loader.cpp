e0901b44a6bd79f9783b46c69663febb225f5bd0
hannes.hofmann@informatik.uni-erlangen.de <hannes.hofmann@informatik.uni-erlangen.de@8ec13cb8-8492-937a-4ef0-9464e09456c2>
2011-02-22 16:40:42 +0000

    Added functionality for "Prepare" functions to rct_module_loader.

--- trunk/src/RabbitCTRunner/source/rct_module_loader.cpp
+++ trunk/src/RabbitCTRunner/source/rct_module_loader.cpp
@@ -9,10 +9,11 @@ using namespace std;
 
 // global variables
 // 
-TfncLoadAlgorithm	s_fncLoadAlgorithm;
-TfncUnloadAlgorithm	s_fncUnloadAlgorithm;
-TfncUnloadAlgorithm	s_fncFinishAlgorithm;
-TfncAlgorithmIteration	s_fncAlgorithmIteration;
+TfncLoadAlgorithm      s_fncLoadAlgorithm;
+TfncPrepareAlgorithm   s_fncPrepareAlgorithm;
+TfncAlgorithmIteration s_fncAlgorithmIteration;
+TfncFinishAlgorithm    s_fncFinishAlgorithm;
+TfncUnloadAlgorithm    s_fncUnloadAlgorithm;
 
 
 #ifdef __RCT_OS_WINDOWS
@@ -31,33 +32,39 @@ bool loadSharedLibraryFunctions(const char * slpath)
 {
 	s_mod = LoadLibrary(slpath);
 
-	if (s_mod == NULL)
-	{
+	if (s_mod == NULL) {
 		cerr << "Failed to load algorithm module " << slpath << endl;
 		return false;
 	}
 
-	if (!(s_fncLoadAlgorithm = (TfncLoadAlgorithm) GetProcAddress(s_mod, RCT_FNCN_LOADALGORITHM)))
-	{
-		cerr << "Failed to locate module function: " << RCT_FNCN_LOADALGORITHM << endl;
+	s_fncLoadAlgorithm = (TfncLoadAlgorithm) GetProcAddress(s_mod, RCT_FNCN_LOADALGORITHM);
+	if (!s_fncLoadAlgorithm) {
+		cerr << "Failed to locate function " << RCT_FNCN_LOADALGORITHM << " in module." << endl;
 		return false;
 	}
 
-	if (!(s_fncUnloadAlgorithm = (TfncUnloadAlgorithm) GetProcAddress(s_mod, RCT_FNCN_UNLOADALGORITHM)))
-	{
-		cerr << "Failed to locate module function: " << RCT_FNCN_UNLOADALGORITHM << endl;
+	s_fncPrepareAlgorithm = (TfncPrepareAlgorithm) GetProcAddress(s_mod, RCT_FNCN_PREPAREALGORITHM);
+	if (!s_fncPrepareAlgorithm) {
+#ifdef _DEBUG
+		cout << "Notice: Function " << RCT_FNCN_PREPAREALGORITHM << " not found in module. Continuing without it." << endl;
+#endif // _DEBUG
+	}
+
+	s_fncAlgorithmIteration = (TfncAlgorithmIteration) GetProcAddress(s_mod, RCT_FNCN_ALGORITHMBACKPROJ);
+	if (!s_fncAlgorithmIteration) {
+		cerr << "Failed to locate function " << RCT_FNCN_ALGORITHMBACKPROJ << " in module." << endl;
 		return false;
 	}
 
-	if (!(s_fncFinishAlgorithm = (TfncFinishAlgorithm) GetProcAddress(s_mod, RCT_FNCN_FINISHALGORITHM)))
-	{
-		cerr << "Failed to locate module function: " << RCT_FNCN_FINISHALGORITHM << endl;
+	s_fncFinishAlgorithm = (TfncFinishAlgorithm) GetProcAddress(s_mod, RCT_FNCN_FINISHALGORITHM);
+	if (!s_fncFinishAlgorithm) {
+		cerr << "Failed to locate function " << RCT_FNCN_FINISHALGORITHM << " in module." << endl;
 		return false;
 	}
 
-	if (!(s_fncAlgorithmIteration = (TfncAlgorithmIteration) GetProcAddress(s_mod, RCT_FNCN_ALGORITHMBACKPROJ)))
-	{
-		cerr << "Failed to locate module function: " << RCT_FNCN_ALGORITHMBACKPROJ << endl;
+	s_fncUnloadAlgorithm = (TfncUnloadAlgorithm) GetProcAddress(s_mod, RCT_FNCN_UNLOADALGORITHM);
+	if (!s_fncUnloadAlgorithm) {
+		cerr << "Failed to locate function " << RCT_FNCN_UNLOADALGORITHM << " in module." << endl;
 		return false;
 	}
 
@@ -66,8 +73,7 @@ bool loadSharedLibraryFunctions(const char * slpath)
 
 void releaseSharedLibrary()
 {
-	if (s_mod)
-	{
+	if (s_mod) {
 		FreeLibrary(s_mod);
 		s_mod = NULL;
 	}
@@ -84,8 +90,7 @@ bool loadSharedLibraryFunctions(const char * slpath)
 {
 	s_mod = dlopen(slpath, RTLD_LAZY);
 
-	if (!s_mod)
-	{
+	if (!s_mod) {
 		cerr << "Failed to load algorithm module " << slpath << endl;
 		return false;
 	}
@@ -94,27 +99,29 @@ bool loadSharedLibraryFunctions(const char * slpath)
 	dlerror();
 
 
-	if (!(s_fncLoadAlgorithm = (TfncLoadAlgorithm) dlsym(s_mod, RCT_FNCN_LOADALGORITHM)))
-	{
-		cerr << "Failed to locate module function: " << RCT_FNCN_LOADALGORITHM << endl;
+	if (!(s_fncLoadAlgorithm = (TfncLoadAlgorithm) dlsym(s_mod, RCT_FNCN_LOADALGORITHM))) {
+		cerr << "Failed to locate function " << RCT_FNCN_LOADALGORITHM << " in module." << endl;
 		return false;
 	}
 
-	if (!(s_fncUnloadAlgorithm = (TfncUnloadAlgorithm) dlsym(s_mod, RCT_FNCN_UNLOADALGORITHM)))
-	{
-		cerr << "Failed to locate module function: " << RCT_FNCN_UNLOADALGORITHM << endl;
+	if (!(s_fncPrepareAlgorithm = (TfncPrepareAlgorithm) dlsym(s_mod, RCT_FNCN_PREPAREALGORITHM))) {
+#ifdef _DEBUG
+		cout << "Notice: Function " << RCT_FNCN_PREPAREALGORITHM << " not found in module. Continuing without it." << endl;
+#endif // _DEBUG
+	}
+
+	if (!(s_fncAlgorithmIteration = (TfncAlgorithmIteration) dlsym(s_mod, RCT_FNCN_ALGORITHMBACKPROJ))) {
+		cerr << "Failed to locate function " << RCT_FNCN_ALGORITHMBACKPROJ << " in module." << endl;
 		return false;
 	}
 
-	if (!(s_fncFinishAlgorithm = (TfncFinishAlgorithm) dlsym(s_mod, RCT_FNCN_FINISHALGORITHM)))
-	{
-		cerr << "Failed to locate module function: " << RCT_FNCN_FINISHALGORITHM << endl;
+	if (!(s_fncFinishAlgorithm = (TfncFinishAlgorithm) dlsym(s_mod, RCT_FNCN_FINISHALGORITHM))) {
+		cerr << "Failed to locate function " << RCT_FNCN_FINISHALGORITHM << " in module." << endl;
 		return false;
 	}
 
-	if (!(s_fncAlgorithmIteration = (TfncAlgorithmIteration) dlsym(s_mod, RCT_FNCN_ALGORITHMBACKPROJ)))
-	{
-		cerr << "Failed to locate module function: " << RCT_FNCN_ALGORITHMBACKPROJ << endl;
+	if (!(s_fncUnloadAlgorithm = (TfncUnloadAlgorithm) dlsym(s_mod, RCT_FNCN_UNLOADALGORITHM))) {
+		cerr << "Failed to locate function " << RCT_FNCN_UNLOADALGORITHM << " in module." << endl;
 		return false;
 	}
 
@@ -123,8 +130,7 @@ bool loadSharedLibraryFunctions(const char * slpath)
 
 void releaseSharedLibrary()
 {
-	if (s_mod)
-	{
+	if (s_mod) {
 // Intel icpc does not unload dyn libs, if they have threads running
 // This is the case for OpenMP!
 // Since unloading the lib is the very last action of RabbitCTRunner it's fine to skip it.
