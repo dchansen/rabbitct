713aac892beed5020c7d97ccb32ce6cb6060632e
hannes.hofmann@informatik.uni-erlangen.de <hannes.hofmann@informatik.uni-erlangen.de@8ec13cb8-8492-937a-4ef0-9464e09456c2>
2011-02-22 16:35:00 +0000

    Minor changes to rct_timer.*

--- trunk/src/RabbitCTRunner/include/rct_timer.h
+++ trunk/src/RabbitCTRunner/include/rct_timer.h
@@ -7,26 +7,25 @@
 
 
 #ifdef __RCT_OS_WINDOWS
+	#include <windows.h>
 
-#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
-    #define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
-#else
-    #define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
-#endif
+	#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
+		#define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
+	#else
+		#define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
+	#endif
 
-struct timezone 
-{
-	int  tz_minuteswest; /* minutes W of Greenwich */
-	int  tz_dsttime;     /* type of dst correction */
-};
+	struct timezone {
+		int  tz_minuteswest; /* minutes W of Greenwich */
+		int  tz_dsttime;     /* type of dst correction */
+	};
 
-int gettimeofday(struct timeval *tv, struct timezone *tz);
-
+	int gettimeofday(struct timeval* tv, struct timezone* tz);
 #endif // __RCT_OS_WINDOWS
 
 
 #ifdef __RCT_OS_LINUX
-// empty
+	// empty
 #endif // __RCT_OS_LINUX
 
 
